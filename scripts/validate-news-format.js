2335ee25f23de1664321d84ca63aba4331722c9d
Filip Sodić <filip.sodic@fer.hr>
2025-12-29 19:05:12 +0100

    Add explanation for title size limit

--- scripts/validate-news-format.js
+++ scripts/validate-news-format.js
@@ -5,8 +5,14 @@ import { z } from "zod";
 // types in `Wasp.Cli.Command.News.Core` in `wasp-lang/wasp` repo.
 const newsItemSchema = z.strictObject({
   id: z.string(),
-  // Title max length is related to the max line length of 80, for details
-  // check the `Wasp.Cli.Command.News.Display` module in `wasp-lang/wasp` repo.
+  // Title's max length comes from how Wasp CLI prints the news.
+  // In short:
+  //   - Maximum line length when displaying the news is 80.
+  //   - The formatting (date, separators, etc.) take some space (around 16
+  //   characters at the time of writing).
+  //   - We limit the title to 60 characters to ensure everything fits nicely.
+  // Check the `Wasp.Cli.Command.News.Display` module in `wasp-lang/wasp` to
+  // understand more.
   title: z.string().max(60),
   body: z.string(),
   level: z.enum(["critical", "important", "info"]),
